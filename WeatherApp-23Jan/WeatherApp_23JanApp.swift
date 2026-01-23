d9a5c251f449da79f9343cb1d3a952a0a809081e
vaibhav indure <indurevaibhav9@gmail.com>
2026-01-22 22:43:23 -0800

    Initial Commit

---
+++ WeatherApp-23Jan/WeatherApp_23JanApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_23JanApp.swift
+//  WeatherApp-23Jan
+//
+//  Created by rentamac on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_23JanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
